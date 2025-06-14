2dcb19cee92937a261f6bfec1a6df701673de6bf
andyckp <andy.kp.chan@gmail.com>
2025-06-14 16:45:06 +0100

    #add trade processors

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,7 @@
-use aeron_rs::context::Context;
-use rs_trade_processor::start_server; // Use the function from lib.rs
-
-// fn main() {
-//     start_server();
-// }
-
-
-
-use aeron_rs::aeron::Aeron;
-use aeron_rs::subscription::Subscription;
-use std::ffi::CString;
-use std::thread::sleep;
-// use bincode;
-// use Trade;
-
-const STREAM_ID: i32 = 1001;
-const CHANNEL: &str = "aeron:ipc";  // Shared memory transport
+mod data_structure;
+mod data;
+mod trade_processor;
 
 fn main() {
-    let mut context = Context::new();
-//    context.set_aeron_dir(CString::new("/tmp/aeron").unwrap());
-    context.set_aeron_dir("/tmp/aeron1".to_string());
-    // context.set_term_buffer_length(1048576);
-    let mut aeron = Aeron::new(context).expect("Failed to initialize Aeron");
-   
-    let channel = CString::new("aeron::ipc").expect("Failed to create CString");
-   
-    let subscription = aeron.add_subscription(channel, STREAM_ID)
-        .expect("Failed to subscribe to IPC channel");
-
-    // loop {
-    //     if let Ok(packet) = subscription.poll() {
-    //         if let Ok(decoded_trade) = bincode::deserialize::<Trade>(&packet) {
-    //             println!("Processed trade: {:?}", decoded_trade);
-    //         }
-    //     }
-    //     sleep(Duration::from_millis(100));
-    // }
+	// Entry point of the application
 }
--- tests/server_test.rs
+++ tests/server_test.rs
@@ -1,10 +1,25 @@
+use aeron_rs::context::Context;
+
+use aeron_rs::aeron::Aeron;
+use aeron_rs::subscription::Subscription;
+use std::ffi::CString;
+use std::thread::sleep;
+
+
+const STREAM_ID: i32 = 1001;
+const CHANNEL: &str = "aeron:ipc";  // Shared memory transport
+
 #[cfg(test)]
-mod test {
-    use rs_trade_processor::start_server;
+#[test]
+fn test_aeron_client() {
+    let mut context = Context::new();
+//    context.set_aeron_dir(CString::new("/tmp/aeron").unwrap());
+    context.set_aeron_dir("/tmp/aeron1".to_string());
+    // context.set_term_buffer_length(1048576);
+    let mut aeron = Aeron::new(context).expect("Failed to initialize Aeron");
 
-    #[test]
-    fn test_server_startup() {
-        start_server(); // Call function to ensure it runs
-    }
+    let channel = CString::new("aeron::ipc").expect("Failed to create CString");
+
+    let subscription = aeron.add_subscription(channel, STREAM_ID)
+        .expect("Failed to subscribe to IPC channel");
 }
-
